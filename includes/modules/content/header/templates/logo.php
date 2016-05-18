fc7d45cfa98e599d7ba64c885f7dd1ef38d1f0e1
John Ny <kom.huy@gmail.com>
2016-05-18 14:47:08 +0700

    update style

--- includes/modules/content/header/templates/logo.php
+++ includes/modules/content/header/templates/logo.php
@@ -1,99 +1,50 @@
-<nav class="navbar" role="navigation">
-  <div class="container" >
-      <table width="100%">
-          <tr>
-              <td>
-                  <div class="col-sm-12">
-                      <?php
-                      echo '<a href="' . tep_href_link('index.php') . '">
-                            <img src="' . DIR_WS_IMAGES . STORE_LOGO .'"class="img-responsive" style="height: 104px;"/></a>';
-                      ?>
-                  </div>
-              </td>
-          </tr>
-      </table>
+<nav class = "navbar navbar-default" role = "navigation">
 
-      <div class="navbar-header">
+    <div class = "navbar-header">
+        <button type = "button" class = "navbar-toggle"
+                data-toggle = "collapse" data-target = "#example-navbar-collapse">
+            <span class = "sr-only">Toggle navigation</span>
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+            <span class = "icon-bar"></span>
+        </button>
 
-          <button
-              type="button"
-              class="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#navbar"
-              style="background: rgb(220,190,126);border: 0px;"
-              >
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-          </button>
-      </div>
-      <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
-          <?php
-          echo tep_get_categories_list();
-          ?>
-      </div>
-      <?php /*
-    <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
-      <ul class="nav navbar-nav navbar-right">
-          <li>
-            <a href="advanced_search.php">Advance Search</a>
-          </li>
-          <li>
-            <a href="account.php#/index">Manage Ad</a>
-          </li>
-          <li class="dropdown">
-            <?php
-            if (tep_session_is_registered('customer_id')) {
-              ?>
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <span class="glyphicon glyphicon-user"></span>
-                <?php echo $_SESSION['user_name']?>
-                <b class="caret"></b>
-              </a>
-              <?php
-            }else{
-              ?>
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <b class="caret"></b></a>
-              <?php
-            }
-            ?>
-            <ul class="dropdown-menu">
-              <?php
-              if (tep_session_is_registered('customer_id')) {
-                echo '<li><a href="' . tep_href_link(FILENAME_LOGOFF, '', 'SSL') . '">' . HEADER_ACCOUNT_LOGOFF . '</a>';
-              }
-              else {
-                echo '<li><a href="' . tep_href_link(FILENAME_LOGIN, '', 'SSL') . '">' . HEADER_ACCOUNT_LOGIN . '</a>';
-                echo '<li><a href="' . tep_href_link(FILENAME_CREATE_ACCOUNT, '', 'SSL') . '">' . HEADER_ACCOUNT_REGISTER . '</a>';
-              }
-              if(tep_session_is_registered('customer_id')){
-                ?>
-<!--                <li class="divider"></li>-->
-<!--                <li>--><?php //echo '<a href="' . tep_href_link(FILENAME_ACCOUNT . '#/account', '', 'SSL') . '">' . HEADER_ACCOUNT . '</a>'; ?><!--</li>-->
-<!--                <li>--><?php //echo '<a href="' . tep_href_link(FILENAME_ACCOUNT . '#/manage', '', 'SSL') . '">Post Ads</a>'; ?><!--</li>-->
-                <?php
-              }
-              ?>
-            </ul>
-          </li>
-      </ul>
-      <div class="col-sm-4" style="float: right">
-        <div class="searchbox-margin">
-          <form name="quick_find"
-                action="advanced_search_result.php"
-                method="get"
-                class="form-horizontal"
-              >
-            <div class="input-group">
-              <input type="search" name="keywords" required="" placeholder="Search" class="form-control">
-                <span class="input-group-btn">
-                  <button type="submit" class="btn btn-info"><i class="glyphicon glyphicon-search"></i></button></span>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div><!--/.nav-collapse -->
- */?>
-  </div><!--/.container-fluid -->
+        <?php
+            echo '<a href="' . tep_href_link('index.php') . '">
+                            <img src="' . DIR_WS_IMAGES . STORE_LOGO .'"class="img-responsive" /></a>';
+        ?>
+    </div>
+
+    <div class ="collapse navbar-collapse" id = "example-navbar-collapse">
+
+        <ul class = "nav navbar-nav navbar-right">
+            <li>
+                <a href = "#">Khmer</a>
+            </li>
+            <li>
+                <a href = "#">English</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class ="collapse navbar-collapse" id = "example-navbar-collapse">
+
+        <ul class = "nav navbar-nav">
+            <li class = "active">
+                <a href = "#">iOS</a>
+            </li>
+            <li>
+                <a href = "#">SVN</a>
+            </li>
+            <li class = "dropdown">
+                <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">
+                    Java
+                </a>
+            </li>
+        </ul>
+        <ul class = "nav navbar-nav navbar-right">
+            <li><input type="text" class="form-control" placeholder="search..."/></li>
+        </ul>
+    </div>
+
 </nav>
